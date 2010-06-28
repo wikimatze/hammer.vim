70b57693ae5b755230e4157928c851969ee839a9
robgleeson <rob@flowof.info>
2010-06-28 06:52:00 +0100

    Remove an older version of vim-markdown-preview if it exists when executing the installer script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "Copying plugin/* to $HOME/.vim/plugin .."
+if [ -e $HOME/.vim/plugin/MKD.vim ]; then
+  echo "Removing old version of vim-markdown-preview ..."
+  rm $HOME/.vim/plugin/MKD.vim
+  echo "DONE.."
+fi
+
+echo "Copying plugin/* to $HOME/.vim/plugin ... "
 cp -R plugin/* $HOME/.vim/plugin
-echo "DONE."
+echo "DONE.."
 
