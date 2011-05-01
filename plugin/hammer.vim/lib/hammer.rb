5fba90acd050ac093a93eaa15c3fd2a521d11168
Rob Gleeson <rob@flowof.info>
2011-05-01 13:37:36 +0100

    Use Hammer::ENV.template_path in Hammer.render

--- plugin/hammer.vim/lib/hammer.rb
+++ plugin/hammer.vim/lib/hammer.rb
@@ -2,8 +2,8 @@ module Hammer
 
   class << self
 
-    def render template
-      template = ERB.new File.read(File.join(Hammer::ENV.template_path, "#{template}.erb.html")) 
+    def render
+      template = ERB.new File.read(File.join(Hammer::ENV.template_path, "#{Hammer::ENV.template}.erb.html")) 
       template.result binding 
     end
 
