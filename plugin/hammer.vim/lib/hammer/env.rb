55dbc30ec85f0ba5baee2d7631b64c24985d522a
Rob Gleeson <rob@flowof.info>
2011-06-03 15:21:39 +0100

    Hammer::ENV.asset_path -> Hammer::ENV.assets_path …

--- plugin/hammer.vim/lib/hammer/env.rb
+++ plugin/hammer.vim/lib/hammer/env.rb
@@ -27,12 +27,12 @@ module Hammer
         @template_path ||= File.join self.base_path, 'templates'
       end
 
-      def asset_path
-        @asset_path ||= File.join self.base_path, 'assets'
+      def assets_path
+        @assets_path ||= File.join self.base_path, 'assets'
       end
 
       def stylesheet_path
-        @stylesheet_path ||= File.join self.asset_path, 'stylesheets'
+        @stylesheet_path ||= File.join self.assets_path, 'stylesheets'
       end
 
     end
