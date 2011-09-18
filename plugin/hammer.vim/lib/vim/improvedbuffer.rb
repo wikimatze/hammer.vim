376c169bc5242b89572c545db8deacfb4f636983
Rob Gleeson <rob@flowof.info>
2011-09-18 07:37:21 +0100

    Add Vim::ImprovedBuffer#to_s.

--- plugin/hammer.vim/lib/vim/improvedbuffer.rb
+++ plugin/hammer.vim/lib/vim/improvedbuffer.rb
@@ -1,38 +1,47 @@
-module Vim
+module Vim::ImprovedBuffer
 
-  module ImprovedBuffer
+  # Adds a more Ruby-like interface to Vim::Buffer#[].
+  #
+  # @param  [Fixnum, Range] key
+  # @return [String]
+  def [] key
+    if key.is_a? Range
+      bufsize  = self.count
+      key      = bufsize - (key.begin).abs .. key.end if key.begin < 0
+      key      = key.begin .. bufsize - (key.end).abs if key.end < 0
 
-    # Adds a more Ruby-like interface to Vim::Buffer#[].
-    #
-    # @param  [Fixnum, Range] key
-    # @return [String]
-    def [] key
-      if key.is_a? Range
-        bufsize  = self.count
-        key      = bufsize - (key.begin).abs .. key.end if key.begin < 0
-        key      = key.begin .. bufsize - (key.end).abs if key.end < 0
-
-        key.map { |number| super number + 1 }.join "\n"
-      else
-        super number + 1
-      end
+      key.map { |number| super number + 1 }.join "\n"
+    else
+      super number + 1
     end
+  end
 
-    # @return [String] Returns the filename of the buffer.
-    def basename
-      File.basename self.name.to_s
-    end
+  # The filename of a buffer.
+  #
+  # @return [String]
+  def basename
+    File.basename self.name.to_s
+  end
 
-    # @return [String] Returns the file extension of the buffer.
-    def extname
-      File.extname self.name.to_s
-    end
+  # The file extension of a buffer.
+  #
+  # @return [String]
+  def extname
+    File.extname self.name.to_s
+  end
 
-    # @return [Boolean] Returns true if the buffer has been saved to disk.
-    def saved?
-      File.exist? self.name.to_s
-    end
+  # Ask the buffer if it exists on disk.
+  # 
+  # @return [Boolean] 
+  def saved?
+    File.exist? self.name.to_s
+  end
 
+  # The contents of the buffer.
+  #
+  # @return [String]
+  def to_s
+    self[0..-1]
   end
 
 end
