7488b7d8554eb654ca66aae3229695e7bb7102bf
Rob Gleeson <rob@flowof.info>
2012-01-17 05:13:24 +0000

    Add markdown custom markdown renderer.

---
+++ plugin/hammer.vim/renderers/markdown_renderer.rb
@@ -0,0 +1,25 @@
+require 'redcarpet'
+
+#
+# Thanks to Steve Klabnik for his blog post about Redcarpet renderers.
+# See http://blog.steveklabnik.com/posts/2011-12-21-redcarpet-is-awesome
+#
+class Hammer::MarkdownRenderer < Redcarpet::Render::HTML
+
+  def block_code code, language
+    if defined?(Albino)
+      Albino.safe_colorize(code, language)
+    else
+      code
+    end
+  end
+
+end
+
+markup :redcarpet, /md|mkd|markdown/ do |content|
+  renderer = Redcarpet::Markdown.new Hammer::MarkdownRenderer.new,
+                                     :tables => true,
+                                     :fenced_code_blocks => true,
+                                     :auto_linl => true
+  renderer.render(content)
+end
