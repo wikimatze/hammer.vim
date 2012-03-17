cdc8d4a2172d83a4d43fb843f7c48afb1840a9cf
Rob Gleeson <rob@flowof.info>
2012-03-17 03:04:57 +0000

    Delay require of 'redcarpet'.
    Avoids a LoadError being raised at boot time for users who do not have
    the redcarpet gem installed. Everyone isn't using hammer for markdown so
    forcing people to install redcarpet(which they may never use) is unreasonable.

--- plugin/hammer.vim/renderers/markdown_renderer.rb
+++ plugin/hammer.vim/renderers/markdown_renderer.rb
@@ -1,21 +1,9 @@
-require 'redcarpet'
-
-#
-# Thanks to Steve Klabnik for his blog post about Redcarpet renderers.
-# See http://blog.steveklabnik.com/posts/2011-12-21-redcarpet-is-awesome
-#
-class Hammer::MarkdownRenderer < Redcarpet::Render::HTML
-
-=begin
-  # TODO: implement me
-  def block_code code, language
-    code
-  end
-=end
-
-end
-
 markup :redcarpet, /md|mkd|markdown|mdwn/ do |content|
+  require 'redcarpet'
+  
+  class Hammer::MarkdownRenderer < Redcarpet::Render::HTML
+  end
+
   renderer = Redcarpet::Markdown.new Hammer::MarkdownRenderer.new,
                                      :tables => true,
                                      :fenced_code_blocks => true,
